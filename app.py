5ae45bcbb59c885a32508e1a406b5243a2cbd5ce
abdoulahadi <88839248+abdoulahadi@users.noreply.github.com>
2024-05-03 21:04:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from fastapi import FastAPI, UploadFile, File
-from pydantic import BaseModel
+from fastapi import FastAPI, UploadFile
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from io import BytesIO
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse
+from datetime import datetime
+import os
+import uuid
 
 app = FastAPI()
-
-class ImageUpload(BaseModel):
-    file: bytes
 
 def calculate_ndvi(red_band, nir_band):
     red_band[red_band == 0] = np.nan
@@ -17,9 +16,15 @@ def calculate_ndvi(red_band, nir_band):
     ndvi = (nir_band - red_band) / (nir_band + red_band)
     return ndvi
 
+def save_image_to_disk(image_bytes):
+    unique_filename = f"img/{datetime.now().strftime('%Y%m%d%H%M%S')}_{uuid.uuid4().hex}.png"
+    with open(unique_filename, "wb") as f:
+        f.write(image_bytes)
+    return unique_filename
+
 @app.post("/process-image/")
 async def process_image(image: UploadFile, recipient_email: str = None):
-    img = Image.open(image.file).convert('RGB')
+    img = Image.open(BytesIO(await image.read())).convert('RGB')
     img_array = np.array(img)
 
     # Extracting red and near-infrared bands
@@ -35,4 +40,7 @@ async def process_image(image: UploadFile, recipient_email: str = None):
     buf.seek(0)
     plt.close()
 
-    return StreamingResponse(buf, media_type="image/png")
+    # Save image to disk
+    filename = save_image_to_disk(buf.getvalue())
+
+    return {"image": f"https://web-production-8333.up.railway.app/{filename}"}
